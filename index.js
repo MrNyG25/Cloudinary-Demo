3d4c4706337fd76e2eca627729959bd66c5dd34d
desarrollo5 <ingeniero.desarrollo5@comersantander.com>
2024-03-19 08:33:01 -0500

    upload multiple files

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require('path');
 const ejs = require('ejs');
 const port = 8080;
+const cloudinary = require('cloudinary').v2;
+
 
 const { storage, getImage } = require('./storage/storage');
 const multer = require('multer');
@@ -23,6 +25,26 @@ app.get('', (req, res) => {
  app.post('/upload', upload.single('image'), (req, res) => {
   console.log(req.file);
   res.send('Done');
+});
+
+app.post("/images", upload.array("pictures", 10), async (req, res) => {
+  try {
+    let pictureFiles = req.files;
+    //Check if files exist
+    if (!pictureFiles)
+      return res.status(400).json({ message: "No picture attached!" });
+    //map through images and create a promise array using cloudinary upload function
+    let multiplePicturePromise = pictureFiles.map((picture) =>
+      cloudinary.uploader.upload(picture.path)
+    );
+    // await all the cloudinary upload functions in promise.all, exactly where the magic happens
+    let imageResponses = await Promise.all(multiplePicturePromise);
+    res.status(200).json({ images: imageResponses });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 });
 
 app.get('/img', upload.single('image'), (req, res) => {
